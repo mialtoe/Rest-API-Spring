afe4e83050de0835481fc65e4a5dc12aec582f0d
maltoe <Michael.Altoe@telekom.de>
2021-05-27 15:43:00 +0200

    get (id) und delete Service hinzugefügt

--- src/main/java/de/telekom/rest/controller/PersonController.java
+++ src/main/java/de/telekom/rest/controller/PersonController.java
@@ -5,26 +5,34 @@ package de.telekom.rest.controller;
 import de.telekom.rest.entity.Person;
 import de.telekom.rest.repository.PersonRepository;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class PersonController {
-
+	    
+	
 	    @Autowired
         PersonRepository personRepository;	
 	
+	    
+	    
 	    @RequestMapping(	        
 	    		   method = RequestMethod.GET,
 	    		   path = "/person",
@@ -39,6 +47,49 @@ public class PersonController {
 	    		    	
 	    }
 	    
+	        
+	    @RequestMapping(	        
+	    		   method = RequestMethod.GET,
+	    		   path = "/person/{id}",
+	    		   produces = MediaType.APPLICATION_JSON_VALUE
+	    )
+
+		@GetMapping ("/{id}")                // gibt die Person mit der ID zurück
+	    public Person getPersonById(@PathVariable String id) {
+	    	int idInt = Integer.parseInt(id);
+	    	
+//	    	Person einePerson = personRepository.getById(idInt);  // geht nicht, wirft exception!!
+	    	try { 
+	    	  Person einePerson = personRepository.findById(idInt).get();
+	    	  return einePerson;
+	    	} catch (Exception Exception) {
+	    		System.out.println(String.format("Person mit ID: %s nicht gefunden!",id));
+	    		return null;
+	    	} 
+	    }
+
+	    
+	    @RequestMapping(	        
+	    		   method = RequestMethod.DELETE,
+	    		   path = "/person/{id}",
+	    		   produces = MediaType.APPLICATION_JSON_VALUE
+	    )
+
+		@DeleteMapping ("/{id}")                // löscht Person mit der ID 
+	    public  HttpStatus delPersonById(@PathVariable String id) {
+	    	int idInt = Integer.parseInt(id);
+ 	    	
+	    	try { 
+	    	  personRepository.deleteById(idInt);
+	    	  return HttpStatus.OK;
+	    	} catch (Exception Exception) {
+	    		System.out.println(String.format("Person mit ID: %s zum Löschen nicht gefunden!",id));
+	    		return HttpStatus.NO_CONTENT;
+	    	} 
+	    }
+
+	    
+	    
 	    @RequestMapping(	        
 	    		   method = RequestMethod.POST,
 	    		   path = "/person",
@@ -50,5 +101,8 @@ public class PersonController {
 	    	personRepository.save(p);
 	    	return new ResponseEntity<>(p, HttpStatus.CREATED);
 	    }
+	    
+	    
+	    
 	    
 }
--- src/main/java/de/telekom/rest/entity/Person.java
+++ src/main/java/de/telekom/rest/entity/Person.java
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import de.telekom.rest.lookup.*;
+import de.telekom.rest.lookup.Salutation;
 
 
 
@@ -17,29 +17,32 @@ public class Person {
 	 
 	  @Id
 	  @GeneratedValue(strategy = GenerationType.IDENTITY)
-	  private Long id;
+	  private Integer id;
 	  
 	  private String vorname;
 	  private String nachname;
-	  private Salutation anrede;
+	  private byte anrede;
 	  
-	  public Salutation getAnrede() {
+	  public byte getAnrede() {
+//		  System.out.println("XXX"+Salutation.fromByte(this.anrede));
 		 return anrede;
 	  }
 
-	  public void setAnrede(byte b) {
-		 this.anrede = Salutation.fromByte(b);
+	  public void setAnrede(byte anrede) {
+		 this.anrede = anrede;
+//		 System.out.println("YYYY"+Salutation.fromByte(this.anrede));
 	  }
 
 	
 	  public Person () {
       }
 	  
-	  public Person(String vname, String nname, Salutation anrede) {
+	  public Person(String vname, String nname, byte anrede) {
 		  this.vorname = vname;
 		  this.nachname=nname;
 		  this.anrede = anrede;
-	  }
+		  System.out.println("ZZZZ"+this.anrede);
+		  }
 	
 	  public String getVorname() {
 		  return vorname;
@@ -56,11 +59,11 @@ public class Person {
 		  this.nachname =  nname;
 	  }
 
-	  public Long getId() {
+	  public Integer getId() {
 		return id;
 	  }
 
-	  public void setId(Long id) {
+	  public void setId(Integer id) {
 		this.id = id;
 	  }
 	  
